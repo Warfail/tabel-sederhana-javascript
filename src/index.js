beaa20d9b17ab3feab4411241fbcf530cffd0a15
Ikky <rezkytaewa2@gmail.com>
2022-03-02 16:54:55 +0700

    percobaan ke3

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-	var Data = [
-	    {'nama':'Rezky', 'email':'rezky@bot.com', 'telepon':'(353) 01 222 3333"'},
-		{'nama':'Diva', 'email':'diva@gmail.com', 'telepon':'(01) 22 888 4444'},
-		{'nama':'Nando', 'email':'nando@gmail.com', 'telepon':'0097 22 654 00033'},
-	]
+var Data = [
+	["Rezky", "rezky@bot.com", "(353) 01 222 3333"],
+	["Diva", "diva@gmail.com", "(01) 22 888 4444"],
+	["Nando", "nando@gmail.com", "0097 22 654 00033"],
+	
+]
+
+fetch = document.getElementById('fetch');
 
 
 
-	Tabel(Data)
-
-	function Tabel(data){
-		var table = document.getElementById('myTable')
-		table.innerHTML = ' '
-		for (var i = 0; i < data.length; i++){
-			var row = `<tr>
-							<td>${data[i].nama}</td>
-							<td>${data[i].email}</td>
-							<td>${data[i].telepon}</td>
-					  </tr>`
-			table.innerHTML += row
+	for (var i = 0; i < Data.length; i++){
+		var newRow = fetch.insertRow(fetch.length);
+			for (var j = 0; j < Data[i].length; j++){
+				var cell = newRow.insertCell(j);
+				cell.innerHTML = Data[i][j];
+			}
+	
 
 
-		}
 	}
+
